e0fdb652d2e9a4e5d7245cacab4d877c9155de1d
alaaseada <alaaseada87@gmail.com>
2023-09-07 07:55:39 +0300

    Add readExcel

--- Controllers/mainController.js
+++ Controllers/mainController.js
@@ -6,84 +6,130 @@ const Excel = require('exceljs');
 const rootDir = path.dirname(require.main.filename);
 
 const openHome = (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'index.html'));
+  res.status(200).sendFile(path.join(rootDir, 'views', 'index.html'));
 };
 
 const readExcel = async (req, res) => {
-  return res.send({
-    msg: 'Excel file has been read correctly',
-  });
+  try {
+    const dataWorkbook = new Excel.Workbook();
+    const workbook = await dataWorkbook.xlsx.readFile(
+      path.join(rootDir, 'data.xlsx')
+    );
+    const worksheet = dataWorkbook.getWorksheet(1);
+    const columnHeaders = [];
+    const excelRows = [];
+    const row1 = worksheet.getRow(1);
+    row1.eachCell((cell, colNum) =>
+      columnHeaders.push({ header: cell.value, key: cell.value })
+    );
+    worksheet.spliceRows(0, 1);
+    worksheet.eachRow((row, rowNum) => {
+      const item = {};
+      row.eachCell((cell, colNum) => {
+        item[columnHeaders[colNum]?.header] = cell.value;
+      });
+      excelRows.push(item);
+    });
+    return res.status(200).send({
+      msg: 'Excel file has been read correctly',
+      rowCount: worksheet.actualRowCount,
+      data: excelRows,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      msg: `An error occurred. ${error}`,
+    });
+  }
 };
 
 const convertExcelToCSV = async (req, res) => {
-  const dataWorkBook = new Excel.Workbook();
-  const workbook = await dataWorkBook.xlsx.readFile(
-    path.join(rootDir, 'data.xlsx')
-  );
-  await workbook.csv.writeFile(path.join(rootDir, 'converted.csv'), {
-    encoding: 'UTF-8',
-    writeBOM: true,
-  });
-  return res.send({
-    msg: 'Excel file has been successfully converted ino CSV',
-  });
+  try {
+    const dataWorkBook = new Excel.Workbook();
+    const workbook = await dataWorkBook.xlsx.readFile(
+      path.join(rootDir, 'data.xlsx')
+    );
+    await workbook.csv.writeFile(path.join(rootDir, 'converted.csv'), {
+      encoding: 'UTF-8',
+      writeBOM: true,
+    });
+
+    return res.status(200).send({
+      msg: 'Excel file has been successfully converted into CSV',
+    });
+  } catch (error) {
+    return res.status(500).send({
+      msg: `An error occurred. ${error}`,
+    });
+  }
 };
 
 const readCSV = (req, res) => {
-  const stream = fs.createReadStream(path.join(rootDir, 'data.csv'));
-  const csvData = [];
-  let dataRowCount = 0;
-  csv
-    .parseStream(stream, { headers: true, encoding: 'utf-8' })
-    .on('error', (err) =>
-      res.send({
-        msg: err,
-        data: csvData,
-        rowCount: dataRowCount,
+  try {
+    const stream = fs.createReadStream(path.join(rootDir, 'data.csv'));
+    const csvData = [];
+    let dataRowCount = 0;
+    csv
+      .parseStream(stream, { headers: true, encoding: 'utf-8' })
+      .on('error', (err) =>
+        res.send({
+          msg: err,
+          rowCount: dataRowCount,
+          data: csvData,
+        })
+      )
+      .on('data', (row) => {
+        csvData.push(row);
       })
-    )
-    .on('data', (row) => {
-      csvData.push(row);
-    })
-    .on('end', (rowCount) => {
-      dataRowCount = rowCount;
-      return res.send({
-        msg: 'CSV file has been read successfully.',
-        data: csvData,
-        rowCount: dataRowCount,
+      .on('end', (rowCount) => {
+        dataRowCount = rowCount;
+        return res.status(200).send({
+          msg: 'CSV file has been read successfully.',
+          rowCount: dataRowCount,
+          data: csvData,
+        });
       });
+  } catch (error) {
+    return res.status(500).send({
+      msg: `An error occurred. ${error}`,
     });
+  }
 };
 
 const writeCSV = async (req, res) => {
-  const csvStream = csv.format({ headers: true, writeBOM: true });
-  const writeStream = fs.createWriteStream(
-    path.join(rootDir, 'written_from_excel.csv')
-  );
-  csvStream.pipe(writeStream);
-  const dataWorkBook = new Excel.Workbook();
-  const workbook = await dataWorkBook.xlsx.readFile(
-    path.join(rootDir, 'data.xlsx'),
-    { headers: 1 }
-  );
-  const worksheet = workbook.getWorksheet(1);
-  const headerRow = worksheet.getRow(1);
-  const columnHeaders = [];
-  headerRow.eachCell((cell, colNum) => {
-    columnHeaders.push({ header: cell.value, key: cell.value });
-  });
-  worksheet.spliceRows(0, 1);
-  worksheet.eachRow((row, rowNum) => {
-    const item = {};
-    row.eachCell((cell, colNum) => {
-      item[columnHeaders[colNum]?.header] = cell.value;
+  try {
+    const csvStream = csv.format({ headers: true, writeBOM: true });
+    const writeStream = fs.createWriteStream(
+      path.join(rootDir, 'written_from_excel.csv')
+    );
+    csvStream.pipe(writeStream);
+    const dataWorkBook = new Excel.Workbook();
+    const workbook = await dataWorkBook.xlsx.readFile(
+      path.join(rootDir, 'data.xlsx'),
+      { headers: 1 }
+    );
+    const worksheet = workbook.getWorksheet(1);
+    const headerRow = worksheet.getRow(1);
+    const columnHeaders = [];
+    headerRow.eachCell((cell, colNum) => {
+      columnHeaders.push({ header: cell.value, key: cell.value });
     });
-    csvStream.write(item);
-  });
-  csvStream.end();
-  return res.send({
-    msg: 'check the project folder for written_from_excel.csv file',
-  });
+    worksheet.spliceRows(0, 1);
+    worksheet.eachRow((row, rowNum) => {
+      const item = {};
+      row.eachCell((cell, colNum) => {
+        item[columnHeaders[colNum]?.header] = cell.value;
+      });
+      csvStream.write(item);
+    });
+    csvStream.end();
+    return res.status(200).send({
+      msg: 'check the project folder for written_from_excel.csv file',
+    });
+  } catch (error) {
+    return res.status(500).send({
+      msg: `An error occurred. ${error}`,
+    });
+  }
 };
 
 module.exports = {
